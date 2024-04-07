3e6f05e2c27a2882c8511391d2e4dfd20007208d
LiamJones05 <88446020+LiamJones05@users.noreply.github.com>
2024-04-07 18:52:43 +0100

    Button functionality

--- Output.js
+++ Output.js
@@ -3,10 +3,25 @@ const button = document.querySelector("input");
 button.addEventListener("click", outputDetails);
 
 function outputStrength(){
-document.getElementbyId("StrengthOutput").innerHTML = "Password Strength (0-10): 6"
+    var paragraph = document.getElementById("StrengthOutput");
+    paragraph.innerHTML = "Password Strength(0-10): 8"
 
 }
 
-function outputDetails(){
-paragraph.textContent = "Output";
-} 
+
+function StoreDetails() {
+    var URLData = document.getElementById('NewURL').value;
+    var UsernameData = document.getElementById('NewUsername').value;
+    var PasswordData = document.getElementById('NewPassword').value;
+    alert("Following details stored:\nURL: " + URLData +"\nUsername: " + UsernameData + "\nPassword: " + PasswordData);
+    
+}
+
+function ShowData() {
+    /*
+    var URLData = document.getElementById('NewURL').value;
+    var UsernameData = document.getElementById('NewUsername').value;
+    var PasswordData = document.getElementById('NewPassword').value;
+    */ //temp placeholder to get variable values into procedure
+    alert("" + URLData + "\nUsername: " + UsernameData + "\nPassword: " + PasswordData);
+}
